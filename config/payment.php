8819523bee7d5275fa6e9da4a85afa63f99b69ca
ankush-self-projects <ankush.work.messages@gmail.com>
2025-08-11 13:41:13 +0200

    Payments: configure Stripe test keys (EUR) in payment config

--- config/payment.php
+++ config/payment.php
@@ -1,17 +1,14 @@
 <?php
-// Stripe test mode configuration
-// Set your Stripe test keys here or via environment variables
-// export STRIPE_SECRET_KEY=sk_test_xxx
-// export STRIPE_PUBLISHABLE_KEY=pk_test_xxx
+
 
 function envOrDefault(string $key, string $default = ''): string {
     $value = getenv($key);
     return $value !== false && $value !== '' ? $value : $default;
 }
 
-define('STRIPE_SECRET_KEY', envOrDefault('STRIPE_SECRET_KEY', 'sk_test_your_secret_key'));
-define('STRIPE_PUBLISHABLE_KEY', envOrDefault('STRIPE_PUBLISHABLE_KEY', 'pk_test_your_publishable_key'));
+define('STRIPE_SECRET_KEY', envOrDefault('STRIPE_SECRET_KEY', 'sk_test_51RutQxR9zpoadhqvyXiUfVLVSoQPpDZRuoNpsEFacyXna5CAXvN7hDgxQe2caKVG8yOmI7yIi1VIHG1baJQBeeKJ00fLvyFrwG'));
+define('STRIPE_PUBLISHABLE_KEY', envOrDefault('STRIPE_PUBLISHABLE_KEY', 'pk_test_51RutQxR9zpoadhqvHCyWgNx0iVyXiZw4AKIpywyUJR9NKTbzTEIJdGHDfTNVnIqigP2K5dSB57yk1cK9xzJxuRwr00Z2baBsT9'));
 
-// Application currency
+
 define('APP_CURRENCY', 'eur');
 ?> 
